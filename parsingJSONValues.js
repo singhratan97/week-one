0bf4cfcb2d39fd36f756db578b64ae51ab654b26
singhratan97 <74782504+singhratan97@users.noreply.github.com>
2020-11-28 16:41:37 +0100

    JSON task - Parsing an JSON object’s Values:

---
+++ parsingJSONValues.js
@@ -0,0 +1,12 @@
+var object = {
+    name: "RajiniKanth", 
+    age: 33, 
+    hasPets : false
+};
+
+function printAllValues(obj) {
+    
+    console.log(Object.values(obj));
+   }
+
+   printAllValues(object);
